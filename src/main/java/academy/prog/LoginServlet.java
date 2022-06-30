d767de48a4ff0b86f2e7de0493ec40f845375f51
letoja777 <letoja777@gmail.com>
2022-06-30 19:16:44 +0300

    Second commit. Homework 2,3 - add password check (length & difficulty)

--- src/main/java/academy/prog/LoginServlet.java
+++ src/main/java/academy/prog/LoginServlet.java
@@ -2,24 +2,48 @@ package academy.prog;
 
 import jakarta.servlet.http.*;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class LoginServlet extends HttpServlet {
     static final String LOGIN = "admin";
-    static final String PASS = "admin";
+    //static final String PASS = "admin";
+
+    private boolean check(String s){
+        return s.matches("((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%_.,;:?+=]).{10,})");
+    }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String login = request.getParameter("login");
         String password = request.getParameter("password");
+
+        char [] arr_login = login.toCharArray();
+
         String age = request.getParameter("age");
+        char [] arr_age = age.toCharArray();
+
         int intAge = Integer.parseInt(age);
 
-        if (LOGIN.equals(login) && PASS.equals(password)) {
+        if (arr_login.length > 0 && check(password) && intAge > 18) {
             HttpSession session = request.getSession(true);
             session.setAttribute("user_login", login);
             session.setAttribute("user_age", intAge);
+            response.sendRedirect("index.jsp");
+        } else if (arr_login.length > 0 && !check(password) && intAge > 18) {
+            PrintWriter pw = response.getWriter();
+            pw.println("<html><head><title>Prog.kiev.ua Test</title></head>");
+            pw.println("<body><h1>Entered password is uncorrect!</h1></body></html>");
+            pw.println("<br>Click this link to <a href=\"/login?a=exit\">logout</a>");
+        } else if (arr_login.length > 0 && check(password) && intAge < 18) {
+            PrintWriter pw = response.getWriter();
+            pw.println("<html><head><title>Prog.kiev.ua Test</title></head>");
+            pw.println("<body><h1>Your age less than 18! </h1></body></html>");
+            pw.println("<br>Click this link to <a href=\"/login?a=exit\">logout</a>");
+        } else {
+            PrintWriter pw = response.getWriter();
+            pw.println("<html><head><title>Prog.kiev.ua Test</title></head>");
+            pw.println("<body><h1>Uncorrect login or password</h1></body></html>");
+            pw.println("<br>Click this link to <a href=\"/login?a=exit\">logout</a>");
         }
-
-        response.sendRedirect("index.jsp");
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -28,6 +52,8 @@ public class LoginServlet extends HttpServlet {
 
         if ("exit".equals(a) && (session != null)) {
             session.removeAttribute("user_login");
+            session.removeAttribute("user_age");
+            //session.removeAttribute("user_pass");
         }
 
         response.sendRedirect("index.jsp");
--- target/L2LoginEE-1.0-SNAPSHOT/index.jsp
+++ target/L2LoginEE-1.0-SNAPSHOT/index.jsp
@@ -5,7 +5,6 @@
   </head>
   <body>
     <% String login = (String)session.getAttribute("user_login"); %>
-    <% int age = (int) session.getAttribute("user_age"); %>
 
     <% if (login == null || "".equals(login)) { %>
         <form action="/login" method="POST">
@@ -15,12 +14,8 @@
             <input type="submit" />
         </form>
     <% } else { %>
-        <% if (age < 18) { %>
-            <h1>You age less than 18! </h1>
-        <% } %>
         <h1>You are logged in as: <%= login %></h1>
         <br>Click this link to <a href="/login?a=exit">logout</a>
     <% } %>
-
   </body>
 </html>
